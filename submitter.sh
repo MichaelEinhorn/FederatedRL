d9e6a6fa156cb2d9fd0d8b1e3a5e2476815082e3
Einhorn, Michael Ford <meinhorn6@gatech.edu>
2023-01-20 15:54:40 -0500

    Create submitter.sh

---
+++ submitter.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=submitter
+#SBATCH -t1:12:00
+#SBATCH --output=submitter.txt
+#SBATCH -q inferno
+#SBATCH -o outputLogs/submitter.txt
+#SBATCH --mail-user=meinhorn6@gatech.edu
+
+cd $SLURM_SUBMIT_DIR
+module load anaconda3/2022.05
+conda activate torch
+srun python submit.py tExp.sh
