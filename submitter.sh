1f0a4e769b4340b1530a3a502e55a21a1e7473fb
Einhorn, Michael Ford <meinhorn6@gatech.edu>
2023-01-20 16:04:26 -0500

    update sh

--- submitter.sh
+++ submitter.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=submitter
-#SBATCH --account=gts-smaguluri3
-#SBATCH -t1:12:00
-#SBATCH --output=submitter.txt
+#SBATCH -J submitter
+#SBATCH -A gts-smaguluri3
+#SBATCH -t 1-12:00
 #SBATCH -q inferno
 #SBATCH -o outputLogs/submitter.txt
 #SBATCH --mail-user=meinhorn6@gatech.edu
+#SBATCH --mail-type=FAIL
 
 cd $SLURM_SUBMIT_DIR
 module load anaconda3/2022.05
--- tExp.sh
+++ tExp.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=p#
-#SBATCH --account=gts-smaguluri3
-#SBATCH -N1 --ntasks-per-node=f#    # Number of nodes and cores per node required
+#SBATCH -J p#
+#SBATCH -A gts-smaguluri3
+#SBATCH -N1 -nf#    # Number of nodes and cores per node required
 #SBATCH --mem-per-cpu=1G            # Memory per core
-#SBATCH -t3:00             		
-#SBATCH -qembers
-#SBATCH -oReport-outputLogs/p#.txt
+#SBATCH -t 0-3:00:00             		
+#SBATCH -q embers
+#SBATCH -o outputLogs/p#.txt
 #SBATCH --mail-user=meinhorn6@gatech.edu
+#SBATCH --mail-type=FAIL
 
 cd $SLURM_SUBMIT_DIR
 module load anaconda3/2022.05
