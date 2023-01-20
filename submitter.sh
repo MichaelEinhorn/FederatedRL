3458f416222b0c431adf7ac0adee0fea3e009e68
Einhorn, Michael Ford <meinhorn6@gatech.edu>
2023-01-20 16:15:08 -0500

    Update submitter.sh

--- submitter.sh
+++ submitter.sh
@@ -3,7 +3,7 @@
 #SBATCH -A gts-smaguluri3
 #SBATCH -t 1-12:00
 #SBATCH -q inferno
-#SBATCH -o outputLogs/submitter.txt
+#SBATCH -o ~/scratch/logs/submitter.txt
 #SBATCH --mail-user=meinhorn6@gatech.edu
 #SBATCH --mail-type=FAIL
 
